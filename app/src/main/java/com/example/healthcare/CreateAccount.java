10e86cb2f74a91b04a3b6bd1bb8da3d2b45f68cf
vaibhavantil2 <88091026+vaibhavantil2@users.noreply.github.com>
2023-04-11 05:36:52 -0400

    Update CreateAccount.java

--- app/src/main/java/com/example/healthcare/CreateAccount.java
+++ app/src/main/java/com/example/healthcare/CreateAccount.java
@@ -175,7 +175,7 @@ public class CreateAccount extends AppCompatActivity {
                 return;
             }
             System.out.println(CIN); /* <=== */ 
-            Amplitude.getInstance().logEvent("Registration complete", status);
+            Amplitude.getInstance().logEvent("Registration complete for", {firstName, lastName, birthDate ,status});
 
             ld.startLoadingDialog();
             fbAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
